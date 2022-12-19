78f3621b3a4db9e2e2938a1a81ce84b0f069b750
Daniel Lian <89411597+Jinn-Niam@users.noreply.github.com>
2022-12-18 20:34:49 -0500

    Update create_some_files.sh

--- tutorials/basic/task-outputs-to-inputs/create_some_files.sh
+++ tutorials/basic/task-outputs-to-inputs/create_some_files.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 mkdir some-files
+mkdir some-files-alias
 echo "file1" > some-files/file1
 echo "file2" > some-files/file2
 echo "file3" > some-files/file3
 echo "file4" > some-files/file4
+echo "special file" > some-files-alias/special_file
+
 ls
 ls some-files/*
